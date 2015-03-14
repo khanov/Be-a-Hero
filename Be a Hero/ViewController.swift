67c06cf7ce5d2a6a319fc0a874e85b808b032f50
Salavat Khanov <salavat@khanov.com>
2015-03-14 18:14:21 +0500

    Init commit.

---
+++ Be a Hero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Be a Hero
+//
+//  Created by Salavat Khanov on 3/14/15.
+//  Copyright (c) 2015 Arty Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
