546a2a314eebdc9292b295e6e27f688fc2d175fe
Salavat Khanov <salavat@khanov.com>
2015-03-18 18:30:56 +0500

    Add kingdom detail screen and navigation segue.

---
+++ Be a Hero/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  Be a Hero
+//
+//  Created by Salavat Khanov on 3/18/15.
+//  Copyright (c) 2015 Arty Technology. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    var kingdom: Kingdom?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    
+}
--- Be a Hero/ListViewController.swift
+++ Be a Hero/ListViewController.swift
@@ -12,11 +12,17 @@ class ListViewController: UITableViewController {
 
     let dataManager = DataManager()
     
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         NSNotificationCenter.defaultCenter().addObserver(tableView, selector: "reloadData", name: DataManagerDidUpdateDataNotification, object: nil)
         dataManager.updateData()
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(tableView, name: DataManagerDidUpdateDataNotification, object: nil)
     }
     
     @IBAction func logOutButtonDidPress(sender: UIBarButtonItem) {
@@ -28,7 +34,8 @@ class ListViewController: UITableViewController {
         presentViewController(signUpVC, animated: true, completion: nil)
     }
     
-    // Table view data source
+    
+    // MARK: - Table view data source
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataManager.kingdomCount
@@ -42,6 +49,18 @@ class ListViewController: UITableViewController {
         cell.imageView?.image = kingdom.photo
         
         return cell
+    }
+    
+    
+    // MARK: - Navigation
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showKingdomDetails" {
+            if let selectedRow = tableView.indexPathForSelectedRow()?.row {
+                let kingdomDetailViewController = segue.destinationViewController as DetailViewController
+                kingdomDetailViewController.kingdom = dataManager.kingdomAtIndex(selectedRow)
+            }
+        }
     }
     
 }
